8c8a73ac6e3f516980fa5c014c50be2ce9474ecf
Ayushverma6157 <24bcs10700@cuchd.in>
2026-08-20 13:17:52 +0530

    Time: 6 ms (6.5%), Space: 95.1 MB (54.21%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.java
+++ 1701-average-waiting-time/1701-average-waiting-time.java
@@ -3,14 +3,8 @@ class Solution {
         int free=0;
         double waits=0;
         for(int i=0;i<customers.length;i++){
-            if(free<customers[i][0]){
-                waits+=(double)customers[i][1];
-                free=customers[i][0]+customers[i][1];
-            }
-            else{
-                waits+=(double)(free-customers[i][0])+customers[i][1];
-                free+=customers[i][1];
-            }
+           waits+=Math.max(customers[i][0],free)-customers[i][0]+customers[i][1];
+           free=Math.max(customers[i][0],free)+customers[i][1];
         }
         return (double)(waits/customers.length);
     }
