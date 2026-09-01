c0f1205fe6a9c7f669eecea34f13ba84826b1b0f
Ayushverma6157 <24bcs10700@cuchd.in>
2026-09-01 14:56:09 +0530

    Time: 4 ms (99.87%), Space: 106 MB (70.17%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -1,18 +1,8 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
         if(head.next.next==null) return new int[]{-1,-1};
         int firstcp=-1,lastcp=-1,currentcp=-1,mindist=Integer.MAX_VALUE,cc=1;
-        ListNode prev=head,curr=head.next,agla=head.next.next;
+        ListNode prev=head,curr=prev.next,agla=curr.next;
         while(agla!=null){
             if((prev.val>curr.val && agla.val>curr.val) || (prev.val<curr.val && agla.val<curr.val)){
                 if(firstcp==-1) firstcp=cc;
