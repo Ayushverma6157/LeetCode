a807e0586c99225b0883af7039bd5db7a6458afc
Ayushverma6157 <24bcs10700@cuchd.in>
2026-09-16 15:59:51 +0530

    Time: 2 ms (9.12%), Space: 43 MB (64.06%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -4,16 +4,14 @@ class Solution {
             return 0;
         if(s.length()==1 ) return 1;
         HashMap<Integer,Integer> memo=new HashMap<>();
-        System.out.println("Initial calls using "+Integer.parseInt(s.substring(0,1))+" and "+Integer.parseInt(s.substring(0,2)));
         return numDecodings(1,Integer.parseInt(s.substring(0,1)),s,memo)+numDecodings(2,Integer.parseInt(s.substring(0,2)),s,memo);
     }
 
     public int numDecodings(int i, int n,String s, HashMap<Integer, Integer> memo) {
-        System.out.println("i is : "+i+" and n is "+n);
-        if(n<=0 || n>26){System.out.println("n excds"); return 0;}
+        if(n<=0 || n>26)return 0;
         if(i==s.length()) return 1;
         if(s.charAt(i)=='0') return 0;
-        if(i==s.length()-1) {System.out.println("As only one remains return 1");return 1;}
+        if(i==s.length()-1) return 1;
         if(memo.containsKey(i)) return memo.get(i);
         int result=numDecodings(i+1,Integer.parseInt(s.substring(i,i+1)),s,memo)+numDecodings(i+2,Integer.parseInt(s.substring(i,i+2)),s,memo);
         memo.put(i,result);
